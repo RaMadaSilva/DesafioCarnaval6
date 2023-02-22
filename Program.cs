c105f91d09a5f8bb4971d7e4e1cbe67c7d5060aa
RaMadaSilva <raul01mateia@gmail.com>
2023-02-22 12:32:23 +0100

    Criando o dicionario

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿namespace DesafioCarnaval6;
 class Program
 {
+    private static readonly Dictionary<char, char> codigo = new(){{'a', 'K'}, {'b', 'R'}, {'c', 'V'}, {'d', 'A'}, {'e', 'L'}, {'f', 'U'},
+    {'g', 'I'}, {'h', 'R'}, {'i', 'M'}, {'j', 'S'}, {'k', 'P'}, {'l', 'B'}, {'m', 'Y'}, {'n', 'J'}, {'o', 'D'}, {'p', 'C'}, {'q', 'E'},
+    {'r', 'T'}, {'s', 'F'}, {'t', 'W'}, {'u', 'Z'}, {'v', 'G'}, {'w', 'N'}, {'y', 'H'}, {'z', 'O'}, {'0', '#'}, {'1', '!'}, {'2', '$'},
+    {'3', '%'}, {'4', '&'}, {'5', '|'}, {'6', '='}, {'7', '*'}, {'8', '/'}, {'9', '<'}};
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
